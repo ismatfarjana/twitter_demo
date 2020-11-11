77de44bf9685f92c738e119fd492400870646e6f
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-11-11 13:12:57 +1100

    showing up form

--- src/components/posts.js/PostCreate.js
+++ src/components/posts.js/PostCreate.js
@@ -1,10 +1,59 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 
-const PostCreate = () => {
-  return (
-    <div>
-      <div>Create Tweet</div>
-    </div>
-  );
+class PostCreate extends React.Component {
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className="errormsg">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  }
+  //helper method for Field. it will be a controlled element
+  renderInput = ({ input, label, meta }) => {
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} autoComplete="off" />
+        {this.renderError(meta)}
+        {/* <div className="errormsg">{meta.error}</div> */}
+      </div>
+    );
+  };
+
+  //handleing submission
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="form">
+        <Field
+          name="post"
+          component={this.renderInput}
+          label="What's happening?"
+        />
+        <button className="button">Tweeet</button>
+      </form>
+    );
+  }
+}
+
+//validate form input
+const validate = formValues => {
+  const errors = {};
+
+  if (!formValues.post) {
+    // only ran if user didnot give any title
+    errors.post = " ⚠️ You must enter a post!";
+  }
+  return errors;
 };
-export default PostCreate;
+
+export default reduxForm({
+  form: "postCreate",
+  validate
+})(PostCreate);
